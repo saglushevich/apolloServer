3840cdf8bcc9cf1893d970f593ec8fbaafb951a7
Sergey Aglushevich <s.aglushevich5@gmail.com>
2023-04-29 04:19:50 +0300

    fix request

--- src/lambda/datasource.js
+++ src/lambda/datasource.js
@@ -1,18 +1,28 @@
 import { RESTDataSource } from "apollo-datasource-rest";
-import fetch from "node-fetch"
 
 export class DogAPI extends RESTDataSource {
+  constructor() {
+    super()
+    //this is the base url for our API call, if you had more than one async query below this base would be the point where the queries diverge
+    this.baseURL = 'https://api.api-ninjas.com/v1/dogs'
+  }
   async getDog({ breed }) {
-    const response = await fetch(
-      `https://api.api-ninjas.com/v1/dogs?name=${breed}`,
-      {
-        headers: {
-          "X-Api-Key": "FZvdT5D6gVW1zV6qMdv5tw==1H3a9TU340W7LAKZ",
-          "content-type": "application/json"
-        },
-      }
-    );
+    // const response = await fetch(
+    //   `https://api.api-ninjas.com/v1/dogs?name=${breed}`,
+    // {
+    //   headers: {
+    //     "X-Api-Key": "FZvdT5D6gVW1zV6qMdv5tw==1H3a9TU340W7LAKZ",
+    //     "content-type": "application/json"
+    //   },
+    // }
+    // );
 
+    const response = await this.get(`?name=${breed}`, undefined, {
+      headers: {
+        "X-Api-Key": "FZvdT5D6gVW1zV6qMdv5tw==1H3a9TU340W7LAKZ",
+        "content-type": "application/json"
+      },
+    })
     return response;
   }
 }
