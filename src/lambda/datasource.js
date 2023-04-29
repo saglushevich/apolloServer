9ea09251fb02f6210d6654e5b22dd9f3bc54fdde
Sergey Aglushevich <s.aglushevich5@gmail.com>
2023-04-29 03:40:27 +0300

    Add Apollo Server

---
+++ src/lambda/datasource.js
@@ -0,0 +1,20 @@
+/* eslint-disable class-methods-use-this */
+
+import { RESTDataSource } from "apollo-datasource-rest";
+
+export class DogAPI extends RESTDataSource {
+  async getDog({ breed }) {
+    const response = await fetch(
+      `https://api.api-ninjas.com/v1/dogs?name=${breed}`,
+      {
+        headers: {
+          "X-Api-Key": "FZvdT5D6gVW1zV6qMdv5tw==1H3a9TU340W7LAKZ",
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    return data[0];
+  }
+}
