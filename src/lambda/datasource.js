6279ed4ac4a4f0180c889d1c1cd41bd0fda7d25e
Sergey Aglushevich <s.aglushevich5@gmail.com>
2023-04-29 11:14:10 +0300

    update datasource

--- src/lambda/datasource.js
+++ src/lambda/datasource.js
@@ -1,50 +1,48 @@
 import { RESTDataSource } from "apollo-datasource-rest";
 
-export class DogAPI extends RESTDataSource {
-  async getDog({ breed }) {
-    const response = await fetch(
-      `https://api.api-ninjas.com/v1/dogs?name=${breed}`,
-      {
-        headers: {
-          "X-Api-Key": "FZvdT5D6gVW1zV6qMdv5tw==1H3a9TU340W7LAKZ",
-        },
-      }
-    );
-
-    // console.log(response.json());
-    const data = await response.json();
-
-    console.log(data)
-    return data[0];
-  }
-}
-
 // export class DogAPI extends RESTDataSource {
-//   constructor() {
-//     super()
-//     //this is the base url for our API call, if you had more than one async query below this base would be the point where the queries diverge
-//     this.baseURL = 'https://api.api-ninjas.com/v1/dogs'
-//   }
 //   async getDog({ breed }) {
 //     const response = await fetch(
 //       `https://api.api-ninjas.com/v1/dogs?name=${breed}`,
-//     {
-//       headers: {
-//         "X-Api-Key": "FZvdT5D6gVW1zV6qMdv5tw==1H3a9TU340W7LAKZ",
-//         "content-type": "application/json"
-//       },
-//     }
+//       {
+//         headers: {
+//           "X-Api-Key": "FZvdT5D6gVW1zV6qMdv5tw==1H3a9TU340W7LAKZ",
+//         },
+//       }
 //     );
 
-//     // const response = await this.get(`?name=${breed}`, undefined, {
-//     //   headers: {
-//     //     "X-Api-Key": "FZvdT5D6gVW1zV6qMdv5tw==1H3a9TU340W7LAKZ",
-//     //     "content-type": "application/json"
-//     //   },
-//     // })
-
+//     // console.log(response.json());
 //     const data = await response.json();
 
-//     return data;
+//     console.log(data)
+//     return data[0];
 //   }
 // }
+
+export class DogAPI extends RESTDataSource {
+  constructor() {
+    super()
+
+    this.baseURL = 'https://api.api-ninjas.com/v1/dogs'
+  }
+  async getDog({ breed }) {
+    // const response = await fetch(
+    //   `https://api.api-ninjas.com/v1/dogs?name=${breed}`,
+    // {
+    //   headers: {
+    //     "X-Api-Key": "FZvdT5D6gVW1zV6qMdv5tw==1H3a9TU340W7LAKZ",
+    //     "content-type": "application/json"
+    //   },
+    // }
+    // );
+
+    const response = await this.get(`?name=${breed}`, undefined, {
+      headers: {
+        "X-Api-Key": "FZvdT5D6gVW1zV6qMdv5tw==1H3a9TU340W7LAKZ",
+        "content-type": "application/json"
+      },
+    })
+
+    return response[0];
+  }
+}
